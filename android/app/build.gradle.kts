dc08335c6f781db02f59874d4282e4d0fec61e11
melsarargueta-jpg <melsarargueta@gmail.com>
2026-08-27 18:58:35 -0600

    Use modern Gradle DSL to fix compilation and localProperties errors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,34 +4,27 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-val flutterRoot = localProperties.getProperty("flutter.sdk")
-if (flutterRoot == null) {
-    throw GradleException("Flutter SDK not found. Define location with flutter.sdk in the local.properties file.")
-}
-
-val flutterVersionCode = localProperties.getProperty("flutter.versionCode") ?: "1"
-val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
-
 android {
     namespace = "com.example.senda"
     compileSdk = 34
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+        }
     }
 
     defaultConfig {
         applicationId = "com.example.senda"
         minSdk = flutter.minSdkVersion
         targetSdk = 34
-        versionCode = flutterVersionCode.toInt()
-        versionName = flutterVersionName
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
