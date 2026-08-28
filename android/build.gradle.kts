bf0c7fcb0e0647b046f9dfb071ac65b70c2a5806
melsarargueta-jpg <melsarargueta@gmail.com>
2026-08-27 19:31:16 -0600

    Limpiar configuracion y dejar compileSdk en 36 de forma correcta

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,19 +17,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-subprojects {
-    plugins.withId("com.android.application") {
-        extensions.configure<com.android.build.gradle.BaseExtension> {
-            compileSdkVersion(36)
-        }
-    }
-    plugins.withId("com.android.library") {
-        extensions.configure<com.android.build.gradle.BaseExtension> {
-            compileSdkVersion(36)
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
