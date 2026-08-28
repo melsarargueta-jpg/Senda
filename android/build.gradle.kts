2d377015b0177b7e9cba983a97d874768665a672
melsarargueta-jpg <melsarargueta@gmail.com>
2026-08-27 19:38:22 -0600

    Forzar compileSdk 36 en todos los submódulos para solucionar conflicto de ObjectBox

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,6 +17,15 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        val extension = extensions.findByName("android")
+        if (extension != null && extension is com.android.build.gradle.BaseExtension) {
+            extension.compileSdkVersion(36)
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
